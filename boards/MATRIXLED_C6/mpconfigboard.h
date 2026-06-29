a31cb5c3dd369c6802c07dbd76347c5cd3e0773a
Manu <0day1day@users.noreply.github.com>
2026-06-29 10:58:03 +0200

    ci: disable MICROPY_PY_BLUETOOTH in board header (skip nimble sources)

--- boards/MATRIXLED_C6/mpconfigboard.h
+++ boards/MATRIXLED_C6/mpconfigboard.h
@@ -4,3 +4,9 @@
 #define MICROPY_HW_MCU_NAME                 "ESP32C6"
 
 #define MICROPY_HW_ENABLE_UART_REPL         (1)
+
+// Disable the Bluetooth / NimBLE module entirely (this project does not use
+// BLE). Overriding the port default (1) here stops modbluetooth_nimble.c and
+// mpnimbleport.c from being compiled, avoiding the NimBLE header dependency.
+#define MICROPY_PY_BLUETOOTH                (0)
+#define MICROPY_BLUETOOTH_NIMBLE            (0)
